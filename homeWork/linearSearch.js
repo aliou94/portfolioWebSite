ee1a940d677b668f1012f2265d9af472e780ee4e
ALI <ahmedaliou94@gmail.com>
2021-08-03 19:28:47 -0400

    LinearSearchH/W

---
+++ homeWork/linearSearch.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+const userInput = prompt('enter a number');
+
+
+let array = [2, 3 , 4, 11 , 12, 5,5,5, 2, 16, 9, 1]
+var position = []
+
+for(let i=0; i<array.length; i++){
+  
+ if (array[i]===parseFloat(userInput)) position.push(i)
+   
+}
+
+console.log(`userInput:${userInput} it occurrs ${position.length} times at index ${position}`)
